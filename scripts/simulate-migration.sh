2d7c517781cf0488c3624f7e4d8e9e017b578c2f
Eli James <116235057+elijamesku@users.noreply.github.com>
2026-03-22 13:15:28 -0500

    Remove duplicate completion message in migration script
    Removed duplicate completion message and retained a single message.

--- scripts/simulate-migration.sh
+++ scripts/simulate-migration.sh
@@ -12,6 +12,3 @@ gsutil cp "gs://${SOURCE}/test-data/migration-test.txt" "gs://${DEST}/test-data/
 echo "Contents of destination bucket:"
 gsutil ls "gs://${DEST}/"
 echo "Done. Check destination bucket in console or: gsutil ls gs://${DEST}/"
-echo "Done. Check destination bucket in console or: gsutil ls gs://${DEST}/"
-
-
