1d413d4ee2f8dec6f1aa2d8d0bd00dd45a7ef8b8
Eli James <116235057+elijamesku@users.noreply.github.com>
2026-03-21 23:07:31 -0500

    Add completion message to migration script
    Added an extra echo statement to indicate completion.

--- scripts/simulate-migration.sh
+++ scripts/simulate-migration.sh
@@ -12,3 +12,6 @@ gsutil cp "gs://${SOURCE}/test-data/migration-test.txt" "gs://${DEST}/test-data/
 echo "Contents of destination bucket:"
 gsutil ls "gs://${DEST}/"
 echo "Done. Check destination bucket in console or: gsutil ls gs://${DEST}/"
+echo "Done. Check destination bucket in console or: gsutil ls gs://${DEST}/"
+
+
