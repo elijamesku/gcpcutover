a13a5ac3861b07662cbb6fdbe92c1b51ce0900dd
Eli James <116235057+elijamesku@users.noreply.github.com>
2026-03-28 21:23:32 -0500

    Clarify comments in network.tf
    Updated comments for clarity in network.tf

--- terraform/network.tf
+++ terraform/network.tf
@@ -1,8 +1,7 @@
 # ******************************************************************************
 # The foundation of the migration
 # ******************************************************************************
-# Everything lives inside the VPC (Virtual Private Cloud)
-# networking
+# Everything lives inside the VPC (Virtual Private Cloud).
 # ******************************************************************************
 
 # -----------------------------------------------------------------------------
@@ -16,7 +15,6 @@ resource "google_compute_network" "migration_vpc" {
   name                    = "migration-vpc"
   auto_create_subnetworks = false
   routing_mode            = "GLOBAL"
-
   # auto_create_subnetworks = false means WE define subnets (recommended for
   # enterprises to control IP ranges and regional placement).
   # routing_mode = GLOBAL: routes are global (default). REGIONAL is for
