5acd544ce592ad00c6c49fa92bb378e7e87e5814
Eli James <116235057+elijamesku@users.noreply.github.com>
2026-05-04 22:42:47 -0500

    Remove member from storage bucket IAM role
    Remove member assignment from IAM role for storage bucket.

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -20,5 +20,4 @@ resource "google_storage_bucket_iam_member" "transfer_source" {
 resource "google_storage_bucket_iam_member" "transfer_dest" {
   bucket = google_storage_bucket.destination.name
   role   = "roles/storage.objectCreator"
-  member = "serviceAccount:${google_service_account.storage_transfer.email}"
 }
