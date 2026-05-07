825aca80a7430a1aafca21c4ffc0aa66b97107aa
Eli James <116235057+elijamesku@users.noreply.github.com>
2026-05-06 22:08:21 -0500

    Update simulate-migration.sh

--- scripts/simulate-migration.sh
+++ scripts/simulate-migration.sh
@@ -10,3 +10,4 @@ gsutil cp "gs://${SOURCE}/test-data/migration-test.txt" "gs://${DEST}/test-data/
 echo "Contents of destination bucket:"
 gsutil ls "gs://${DEST}/"
 echo "Done. Check destination bucket in console or: gsutil ls gs://${DEST}/"
+echo "Done. Check destination bucket in console or: gsutil ls gs://${DEST}/"
