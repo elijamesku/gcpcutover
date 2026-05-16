0ec0a538d7ca0024d9ad5ed08c163ebc8a424f80
Eli James <116235057+elijamesku@users.noreply.github.com>
2026-05-16 09:37:12 -0500

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -20,4 +20,5 @@ resource "google_storage_bucket_iam_member" "transfer_source" {
 resource "google_storage_bucket_iam_member" "transfer_dest" {
   bucket = google_storage_bucket.destination.name
   role   = "roles/storage.objectCreator"
+  member = "serviceAccount:${google_service_account.storage_transfer.email}"
 }
